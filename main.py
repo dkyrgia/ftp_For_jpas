283a93923c1efc8fbcca31d0ad270f29d628bddc
dkyrgia <dkyrgia@gmail.com>
2023-02-05 15:12:43 +0200

    An external file added to include parameters

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-# This is a sample Python script.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from ftplib import FTP
 import os
+import re
 
-username = "xxxxxxxxxxxxxxx"
-password = "xxxxxxxxxxxxxxx"
-site_url = "abcde.domain.com"
-site_directory = "folder1/folder2/folder3/"
-# jpa_position = -59 # The last N characters of the string which contains the file name
-jpa_position = -42
-string_to_search: str = "panch"
+# initialize the list
+connect_params: list = ["a", "b", "c", "d", 1, "f"]
+# params.txt contains 6 parameters necessary for connection and download
+f = open('params.txt', 'r')
+for x in range(6):
+    temp_param: list = f.readline()
+    connect_params[x] = temp_param
+f.close()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+username = re.sub("\n", "", connect_params[0])
+password = re.sub("\n", "", connect_params[1])
+site_url = re.sub("\n", "", connect_params[2])
+site_directory = re.sub("\n", "", connect_params[3])
+jpa_position = int(re.sub("\n", "", connect_params[4]))
+string_to_search: str = re.sub("\n", "", connect_params[5])
 
 def dirCallback(line):
     line = "%s" % line
@@ -30,8 +31,8 @@ def dirCallback(line):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     cmd = "dir *.jpa | findstr " + string_to_search
-    returned_value_get = os.system(cmd)  # returns a list of all jpa files containing panch
-    cmd = "del *panch*.jpa" # delete all jpa files containing panch
+    returned_value_get = os.system(cmd)  # returns a list of all jpa files containing string_to_search
+    cmd = "del *" + string_to_search + "*.jpa" # delete all jpa files containing string_to_search
     returned_value_del = os.system(cmd)  # returns the exit code in unix
     print('returned value:', returned_value_del)
     ftp = FTP(site_url)  # connect to host, default port
